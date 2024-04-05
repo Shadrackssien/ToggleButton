de9f6441656c441cc6d9e8dc4206544f517f35f3
Shadrackssien <shadrack6960@gmail.com>
2024-04-05 13:52:20 +0000

    Toggle Button js file

---
+++ src/ToggleButon.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./styleToggle.css"
+
+function ToggleButton() {
+    const [backgroundColor, setBackgroundColor] = useState("white");
+    const [textColor, setTextColor] = useState("#1b1b1b");
+
+    const handleClick = () => {
+        setBackgroundColor(backgroundColor === "white" ? "#1b1b1b" : "white");
+        setTextColor(textColor === "#1b1b1b" ? "white" : "#1b1b1b");
+    }
+    return (
+        <section style={{backgroundColor, color: `${textColor}`}}>
+            <button onClick={handleClick} style={{backgroundColor: backgroundColor, color: `${textColor}` }}>White Theme</button>
+            <section className="content">
+                <h1>Welcome to Shadrack Essien's <br/>
+                    Supper Toggle Button
+                </h1>
+            </section>
+        </section>
+    )
+}
+
+export default ToggleButton;
